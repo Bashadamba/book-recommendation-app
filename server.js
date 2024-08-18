c7453d0f1ba4f439ee88df89aee8ba710489cde0
Basha Damba <tuahirud@gmail.com>
2024-08-18 13:48:34 +0000

    Initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
